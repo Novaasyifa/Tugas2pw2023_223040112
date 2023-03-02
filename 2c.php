8a27ca1d484083ee43574d979da1309d28cae2c7
Novaasyifa <126181389+Novaasyifa@users.noreply.github.com>
2023-03-02 02:08:37 -0800

    Add files via upload

---
+++ 2c.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2c.php</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            line-height: 0;
+        }
+
+        .kotak {
+            width: 50px;
+            width: 50px;
+            border: 1px solid black;
+            line-height: 50px;
+            text-align: center;
+            display: inline-block;
+            margin-right: 0px;
+            background: salmon;
+        }
+    </style>
+</head>
+<body>
+    <?php 
+    $jml_kotak= 10;
+    for ($i=$jml_kotak; $i>=1; $i--) {
+        for ($j=1; $j<=$i; $j++) {
+            echo "<div class=\"kotak\">$j</div>";
+        }
+        echo "<br>";
+    }
+    ?>
+</body>
+</html>
